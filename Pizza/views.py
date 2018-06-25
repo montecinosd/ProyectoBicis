d8aebdf3934cc538cca83e49eed590c2f065cbf2
gabriel <holaestoybn@gmail.com>
2018-06-24 20:41:41 -0400

    add_pizza 95%

--- Pizza/views.py
+++ Pizza/views.py
@@ -9,36 +9,29 @@ from django.shortcuts import redirect
 def index(request):
     data = {}
 
-    for i in Pizza.objects.all():
-    	print(i.Ingredients.name)
-
-
 
     template_name = 'index.html'
     ingredient = Ingredients.objects.all()
     data["ing"] = ingredient
     lista_imag = []
     data["mass"] = Mass.objects.all()
-    for i in ingredient:
-    	var = " static '{}' ".format(i.image)
-    	lista_imag.append(var)
-    print(lista_imag)
-    # data["image"]= lista_imag
+
+
     data["total"] = zip(ingredient,lista_imag)
     if request.POST:
     	lista_obj = []
-    	print(request.POST["list_ing"].split(","))
+    	
     	for i in request.POST["list_ing"].split(","):
     		try:
     			lista_obj.append(Ingredients.objects.get(code=i))
     		except:
     			raise
-    	masa = Mass.objects.all()
-    	print(masa)
-    	orden = Pizza(type_mass=masa[0])
+    	masa = Mass.objects.get(code = request.POST["mass"])
+    	
+    	orden = Pizza(type_mass=masa)
+    	orden.save()
     	for i in lista_obj:
-    		orden.Ingredients.add(i)
-    	print(request.POST["list_mass"])
+    		orden.Ingredient.add(i)
     	return JsonResponse({})
     return render(request, template_name, data)
 
